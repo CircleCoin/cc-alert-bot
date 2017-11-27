3cd73ac94764241b099143054361e34cac8ab9d9
Javier Tarazaga <javi.tarazaga@gmail.com>
2017-11-27 09:02:36 +0100

    Added first price retrieval test for eth

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,6 @@
-// import { Response } from "supertest";
+import * as requestService from "request";
+import * as express from "express";
 import { Request, Response } from "express";
-
-"use strict";
 
 const functions = require("firebase-functions"); // Cloud Functions for Firebase library
 const DialogflowApp = require("actions-on-google").DialogflowApp; // Google Assistant helper library
@@ -10,10 +9,8 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request: Requ
   console.log("Dialogflow Request headers: " + JSON.stringify(request.headers));
   console.log("Dialogflow Request body: " + JSON.stringify(request.body));
 
-  if (request.body.result) {
-    processV1Request(request, response);
-  } else if (request.body.queryResult) {
-    processV2Request(request, response);
+  if (request.body.queryResult) {
+    processRequest(request, response);
   } else {
     console.log("Invalid Request");
     return response.status(400).end("Invalid Webhook Request (expecting v1 or v2 webhook request)");
@@ -21,184 +18,9 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request: Requ
 });
 
 /*
-* Function to handle v1 webhook requests from Dialogflow
-*/
-function processV1Request (request: Request, response: Response) {
-  let action = request.body.result.action; // https://dialogflow.com/docs/actions-and-parameters
-  const parameters = request.body.result.parameters; // https://dialogflow.com/docs/actions-and-parameters
-  const inputContexts = request.body.result.contexts; // https://dialogflow.com/docs/contexts
-  const requestSource = (request.body.originalRequest) ? request.body.originalRequest.source : undefined;
-
-  const googleAssistantRequest = "google"; // Constant to identify Google Assistant requests
-  const app = new DialogflowApp({request: request, response: response});
-
-  // Create handlers for Dialogflow actions as well as a 'default' handler
-  const actionHandlers: any = {
-    // The default welcome intent has been matched, welcome the user (https://dialogflow.com/docs/events#default_welcome_intent)
-    "input.welcome": () => {
-      // Use the Actions on Google lib to respond to Google requests; for other requests use JSON
-      if (requestSource === googleAssistantRequest) {
-        sendGoogleResponse("Hello, Welcome to my Dialogflow agent!"); // Send simple response to user
-      } else {
-        sendResponse("Hello, Welcome to my Dialogflow agent!"); // Send simple response to user
-      }
-    },
-    // The default fallback intent has been matched, try to recover (https://dialogflow.com/docs/intents#fallback_intents)
-    "input.unknown": () => {
-      // Use the Actions on Google lib to respond to Google requests; for other requests use JSON
-      if (requestSource === googleAssistantRequest) {
-        sendGoogleResponse("I'm having trouble, can you try that again?"); // Send simple response to user
-      } else {
-        sendResponse("I'm having trouble, can you try that again?"); // Send simple response to user
-      }
-    },
-    // Default handler for unknown or undefined actions
-    "default": () => {
-      // Use the Actions on Google lib to respond to Google requests; for other requests use JSON
-      if (requestSource === googleAssistantRequest) {
-        const responseToUser = {
-          // googleRichResponse: googleRichResponse, // Optional, uncomment to enable
-          // googleOutputContexts: ['weather', 2, { ['city']: 'rome' }], // Optional, uncomment to enable
-          speech: "This message is from Dialogflow's Cloud Functions for Firebase editor!", // spoken response
-          text: "This is from Dialogflow's Cloud Functions for Firebase editor! :-)" // displayed response
-        };
-        sendGoogleResponse(responseToUser);
-      } else {
-        const responseToUser = {
-          // data: richResponsesV1, // Optional, uncomment to enable
-          // outputContexts: [{'name': 'weather', 'lifespan': 2, 'parameters': {'city': 'Rome'}}], // Optional, uncomment to enable
-          speech: "This message is from Dialogflow's Cloud Functions for Firebase editor!", // spoken response
-          text: "This is from Dialogflow's Cloud Functions for Firebase editor! :-)" // displayed response
-        };
-        sendResponse(responseToUser);
-      }
-    }
-  };
-
-  // If undefined or unknown action use the default handler
-  if (!actionHandlers[action]) {
-    action = "default";
-  }
-
-  // Run the proper handler function to handle the request from Dialogflow
-  actionHandlers[action]();
-
-    // Function to send correctly formatted Google Assistant responses to Dialogflow which are then sent to the user
-  function sendGoogleResponse (responseToUser: any) {
-    if (typeof responseToUser === "string") {
-      app.ask(responseToUser); // Google Assistant response
-    } else {
-      // If speech or displayText is defined use it to respond
-      let googleResponse = app.buildRichResponse().addSimpleResponse({
-        speech: responseToUser.speech || responseToUser.displayText,
-        displayText: responseToUser.displayText || responseToUser.speech
-      });
-      // Optional: Overwrite previous response with rich response
-      if (responseToUser.googleRichResponse) {
-        googleResponse = responseToUser.googleRichResponse;
-      }
-      // Optional: add contexts (https://dialogflow.com/docs/contexts)
-      if (responseToUser.googleOutputContexts) {
-        app.setContext(...responseToUser.googleOutputContexts);
-      }
-
-      console.log("Response to Dialogflow (AoG): " + JSON.stringify(googleResponse));
-      app.ask(googleResponse); // Send response to Dialogflow and Google Assistant
-    }
-  }
-  // Function to send correctly formatted responses to Dialogflow which are then sent to the user
-  function sendResponse (responseToUser: any) {
-    // if the response is a string send it as a response to the user
-    if (typeof responseToUser === "string") {
-      const responseJson: any = {};
-      responseJson.speech = responseToUser; // spoken response
-      responseJson.displayText = responseToUser; // displayed response
-      response.json(responseJson); // Send response to Dialogflow
-    } else {
-      // If the response to the user includes rich responses or contexts send them to Dialogflow
-      const responseJson: any = {};
-      // If speech or displayText is defined, use it to respond (if one isn't defined use the other's value)
-      responseJson.speech = responseToUser.speech || responseToUser.displayText;
-      responseJson.displayText = responseToUser.displayText || responseToUser.speech;
-      // Optional: add rich messages for integrations (https://dialogflow.com/docs/rich-messages)
-      responseJson.data = responseToUser.data;
-      // Optional: add contexts (https://dialogflow.com/docs/contexts)
-      responseJson.contextOut = responseToUser.outputContexts;
-
-      console.log("Response to Dialogflow: " + JSON.stringify(responseJson));
-      response.json(responseJson); // Send response to Dialogflow
-    }
-  }
-}
-
-// Construct rich response for Google Assistant (v1 requests only)
-const app = new DialogflowApp();
-const googleRichResponse = app.buildRichResponse()
-  .addSimpleResponse("This is the first simple response for Google Assistant")
-  .addSuggestions(
-    ["Suggestion Chip", "Another Suggestion Chip"])
-    // Create a basic card and add it to the rich response
-  .addBasicCard(app.buildBasicCard(`This is a basic card.  Text in a
- basic card can include "quotes" and most other unicode characters
- including emoji 📱.  Basic cards also support some markdown
- formatting like *emphasis* or _italics_, **strong** or __bold__,
- and ***bold itallic*** or ___strong emphasis___ as well as other things
- like line  \nbreaks`) // Note the two spaces before '\n' required for a
-                        // line break to be rendered in the card
-    .setSubtitle("This is a subtitle")
-    .setTitle("Title: this is a title")
-    .addButton("This is a button", "https://assistant.google.com/")
-    .setImage("https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-      "Image alternate text"))
-  .addSimpleResponse({ speech: "This is another simple response",
-    displayText: "This is the another simple response 💁" });
-
-// Rich responses for Slack and Facebook for v1 webhook requests
-const richResponsesV1 = {
-  "slack": {
-    "text": "This is a text response for Slack.",
-    "attachments": [
-      {
-        "title": "Title: this is a title",
-        "title_link": "https://assistant.google.com/",
-        "text": "This is an attachment.  Text in attachments can include 'quotes' and most other unicode characters including emoji 📱.  Attachments also upport line\nbreaks.",
-        "image_url": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-        "fallback": "This is a fallback."
-      }
-    ]
-  },
-  "facebook": {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "generic",
-        "elements": [
-          {
-            "title": "Title: this is a title",
-            "image_url": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-            "subtitle": "This is a subtitle",
-            "default_action": {
-              "type": "web_url",
-              "url": "https://assistant.google.com/"
-            },
-            "buttons": [
-              {
-                "type": "web_url",
-                "url": "https://assistant.google.com/",
-                "title": "This is a button"
-              }
-            ]
-          }
-        ]
-      }
-    }
-  }
-};
-
-/*
 * Function to handle v2 webhook requests from Dialogflow
 */
-function processV2Request (request: Request, response: Response) {
+function processRequest (request: Request, response: Response) {
   // An action is a string used to identify what needs to be done in fulfillment
   let action = (request.body.queryResult.action) ? request.body.queryResult.action : "default";
   // Parameters are any entites that Dialogflow has extracted from the request.
@@ -222,7 +44,29 @@ function processV2Request (request: Request, response: Response) {
       sendResponse("I'm having trouble, can you try that again?"); // Send simple response to user
     },
     "input.eth": () => {
-      sendResponse("What up. This is another test!");
+      // requestService.get("https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=EUR")
+      // .on("response", function (response: Response) {
+      //   console.log(response.statusCode); // 200
+      //   const apiResponse: any = response;
+      //   console.log(apiResponse);
+      //   const priceEuro = apiResponse.price_eur;
+      //   sendResponse(priceEuro);
+      // })
+      // .on("error", function(err: Error) {
+      //   console.log(err);
+      //   sendResponse("Ups, something went wrong. Please try again!");
+      // });
+      requestService("https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=EUR", function (error: Error, response: Response, body: Body) {
+        // console.log(JSON.parse(body.toString()).price_eur);
+
+        const change24h = JSON.parse(body.toString())[0].percent_change_24h;
+        const price_eur = JSON.parse(body.toString())[0].price_eur;
+
+        let change: string;
+        change24h > 0 ? change = "+" + change24h : change = "-" + change24h;
+
+        sendResponse(`<b>${price_eur}</b><br/>${change}`);
+      });
     },
     // Default handler for unknown or undefined actions
     "default": () => {
